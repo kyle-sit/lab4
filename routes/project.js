3f7a924c877a1db3b1c16f7bf89c3e0879fb63b8
Kyle Sit <ktsit@ucsd.edu>
2018-01-31 11:55:37 -0800

    node.js lab

--- routes/project.js
+++ routes/project.js
@@ -1,6 +1,9 @@
 exports.viewProject = function(req, res) { 
   // controller code goes here 
   var name = req.params.name; 
+  if(name === undefined) {
+  	name = "Blank project";
+  }
   res.render("project", {
   	'projectName' : name
   });
